9ac0f40cca2de734ff58aa4def929188d176462f
“anildalar” <dollorinfotech@gmail.com>
2022-04-08 09:35:56 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createStore } from 'redux';
+
+let rootReducer = (oldState = { value: 'ANIL' },action)=>{
+  let newState = oldState
+  if(action.type === 'abc'){
+      newState.value = 'Abhishesk';
+      return newState;
+  }else if(action.type === 'xyz'){
+      newState.value = 'Avinash';
+      return newState;
+  }else{
+    return newState;
+  } 
+  
+}
+
+ let store = createStore(rootReducer); //Calling
+
+ console.log(store)
+
+ store.subscribe(() => console.log(store.getState()));
+
+
+ store.dispatch({type:"xyz"});
+ store.dispatch({type:"xyz"});
+ store.dispatch({type:"xyz"});
+ store.dispatch({type:"abc"});
+ store.dispatch({type:"abc"});
+ store.dispatch({type:"abc"});
+ store.dispatch({type:"xyz"});
+ store.dispatch({type:"xyz"});
+
+
 function App() {
   return (
     <div className="App">
